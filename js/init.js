4f0dc88291ecef2e6d66b3e5c09b544dd4a35d3e
juzhyo <a0094492@u.nus.edu>
2017-05-31 11:07:47 +0800

    Some layout changes. Added scrollSpy

--- js/init.js
+++ js/init.js
@@ -1,36 +1,41 @@
 (function($){
-  $(function(){
+    $(function(){
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 400,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.cd-top');
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
-
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0 ,
+        }, scroll_top_duration
+                              );
+    });
 });
+
+function initMap() {
+    var uluru = {lat: 1.296751, lng: 103.779304};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 19,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
